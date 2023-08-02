b872c590be2e2a35092618410606c2ebd7193206
vaheeda554 <vaheeda.shaik@mobileprogramming.com>
2023-08-02 14:59:07 +0530

    first commit

---
+++ src/main/java/com/telsuko/demo/Alien.java
@@ -0,0 +1,27 @@
+package com.telsuko.demo;
+
+public class Alien {
+	private int id;
+	private String aname;
+	private String alang;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getAlang() {
+		return alang;
+	}
+	public void setAlang(String alang) {
+		this.alang = alang;
+	}
+	
+
+}
---
+++ src/main/java/com/telsuko/demo/WebProject2Application.java
@@ -0,0 +1,14 @@
+package com.telsuko.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProject2Application.class, args);
+		
+	}
+
+}
